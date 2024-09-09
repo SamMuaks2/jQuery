7dcc53ee3dddfc49ceb4690df61b8783cb3e9676
Mohri Samuel Muakpo <sammuaks@gmail.com>
2024-09-09 15:58:31 +0100

    Add event listeners

--- index.js
+++ index.js
@@ -6,3 +6,27 @@ $("p").text("Dey whine me 🌚");
 
 //Handle inner-html
 $("button").html("<b><em>Ola 👋</em></b>");
+
+// jQuery uses JavaScript's objects and methods
+$("a").attr("href", "www.yahoo.com"); //get the href attribute(object) and set its destination(methods) as Yahoo!.com
+
+// Adding event listeners
+$("h1").click(function() {
+    $("h1").css("color", "yellow");
+});
+
+// Removing for loops
+$("button").click(function() {
+    $("h1").css("color", "indigo");
+});
+
+// Detecting keypresses
+$("input").keypress(function(event) {
+    console.log(event.key);
+    $("h1").text(event.key);
+});
+
+// Shorter method
+$("p").on("mouseover", function() {
+    $("h1").text("😈");
+})
